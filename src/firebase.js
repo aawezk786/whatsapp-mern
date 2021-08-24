2983bbf1752ea8301a41cb97bb24d51a202cf4f9
aawezk456 <aawezk456@gmail.com>
2021-08-24 14:48:27 +0530

    Latest

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiMzapp4iuYoYQEV0R4Xlyi6eWleAVh-I",
+    authDomain: "whatsapp-mern-4b2ae.firebaseapp.com",
+    projectId: "whatsapp-mern-4b2ae",
+    storageBucket: "whatsapp-mern-4b2ae.appspot.com",
+    messagingSenderId: "582178311263",
+    appId: "1:582178311263:web:1f508d161eb2eaecb29fea"
+  };
